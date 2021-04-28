ba6864fb27d1ca564886c9e9ed7346d46e697cee
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-28 10:08:48 -0600

    Create test for trails level helper

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -66,6 +66,28 @@ RSpec.describe Park do
     end
   end
 
+  describe '#trails_level_helper' do
+    it 'returns array of trails by difficulty' do
+      park_1 = Park.new('Capitol Reef')
+      park_2 = Park.new('Bryce Canyon')
+      trail_1 = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
+      trail_2 = Trail.new({name: 'Cohab Canyon', length: '1.7 miles', level: :moderate})
+      trail_3 = Trail.new({name: 'Chimney Rock Loop', length: '3.6 miles', level: :strenuous})
+      trail_4 = Trail.new({name: 'Queens/Navajo Loop', length: '2.9', level: :moderate})
+      trail_5 = Trail.new({name: 'Rim Trail', length: '11 miles', level: :easy})
+      trail_6 = Trail.new({name: 'Tower Bridge', length: '3 miles', level: :moderate})
+
+      park_1.add_trail(trail_1)
+      park_1.add_trail(trail_2)
+      park_1.add_trail(trail_3)
+      park_2.add_trail(trail_4)
+      park_2.add_trail(trail_5)
+      park_2.add_trail(trail_6)
+
+      expect(park_1.trails_level_helper(:easy)).to eq(trail_1)
+    end
+  end
+
   describe '#trails_by_level' do
     it 'returns hash of level equal to matching trails' do
       park_1 = Park.new('Capitol Reef')
