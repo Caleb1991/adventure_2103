7a3a6153c5049d4572c5cb83a73058e3e0ea1604
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-28 09:39:27 -0600

    Create test for possible trails

--- spec/hiker_spec.rb
+++ spec/hiker_spec.rb
@@ -62,4 +62,31 @@ RSpec.describe Hiker do
       expect(hiker.parks_visited).to eq([park])
     end
   end
+
+  describe '#possible_trails' do
+    it 'returns all trails at parks that have been visited and match experience level' do
+      hiker_1 = Hiker.new('Dora', :moderate)
+      park_1 = Park.new('Capitol Reef')
+      park_2 = Park.new('Bryce Canyon')
+      trail_1 = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
+      trail_2 = Trail.new({name: 'Cohab Canyon', length: '1.7 miles', level: :moderate})
+      trail_3 = Trail.new({name: 'Chimney Rock Loop', length: '3.6 miles', level: :strenuous})
+      trail_4 = Trail.new({name: 'Queens/Navajo Loop', length: '2.9', level: :moderate})
+      trail_5 = Trail.new({name: 'Rim Trail', length: '11 miles', level: :easy})
+      trail_6 = Trail.new({name: 'Tower Bridge', length: '3 miles', level: :moderate})
+
+      park_1.add_trail(trail_1)
+      park_1.add_trail(trail_2)
+      park_1.add_trail(trail_3)
+      park_2.add_trail(trail_4)
+      park_2.add_trail(trail_5)
+      park_2.add_trail(trail_6)
+      hiker_1.visit_park(park_1)
+      hiker_1.visit_park(park_2)
+
+      expected = [trail_2, trail_4, trail_6]
+
+      expect(hiker.possible_trails).to eq(expected)
+    end
+  end
 end
