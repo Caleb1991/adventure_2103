0976c874dcc5aa024016dfad75fb15404efb7c47
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-28 09:14:46 -0600

    Create tests for initialize

---
+++ spec/hiker_spec.rb
@@ -0,0 +1,26 @@
+require './lib/hiker'
+require './lib/park'
+require './lib/trail'
+
+RSpec.describe Hiker do
+
+  describe 'initialize' do
+    it 'exists' do
+      hiker = Hiker.new('Dora', :moderate)
+
+      expect(hiker).is_a?(Hiker)
+    end
+
+    it 'has a name' do
+      hiker = Hiker.new('Dora', :moderate)
+
+      expect(hiker.name).to eq('Dora')
+    end
+
+    it 'has an experience level' do
+      hiker = Hiker.new('Dora', :moderate)
+
+      expect(hiker.experience_level).to eq(:moderate)
+    end
+  end
+end
