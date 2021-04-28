1bff646914739b41ae2b7dace44c3b705e1f174a
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-28 10:38:12 -0600

    Remove commented out line

--- lib/trail.rb
+++ lib/trail.rb
@@ -4,7 +4,6 @@ class Trail
               :level
   def initialize(trail_info)
     @name = trail_info[:name]
-    # @length = trail_info[:length].delete(' miles').to_f
     @length = trail_length_conversion(trail_info[:length])
     @level = trail_info[:level]
   end
